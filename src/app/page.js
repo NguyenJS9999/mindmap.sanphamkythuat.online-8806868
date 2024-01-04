7879fa86d8139e45d40b57a62546a45d289e191d
Nguyễn Trung Nguyên <31590813+NguyenKeo@users.noreply.github.com>
2024-01-05 00:19:13 +0700

    change construction file project 2

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { FcGoogle } from 'react-icons/fc';
-import HomePage from './pages/home/HomePage';
+import HomePage from '~/pages/home/HomePage';
 
 export default function Home() {
   return (
